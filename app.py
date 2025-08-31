33803d9462046571a23c7f5c25a5527a3a4443bb
Nupur <146013569+Nupur-tech257@users.noreply.github.com>
2025-08-31 22:59:51 +0530

    push app

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.write("User Data ")
 st.dataframe(df, use_container_width=True)
 
 # Sidebar Filters
-st.sidebar.header("Filter")
+st.sidebar.header("Filters")
 name_filter = st.sidebar.text_input("Search by Name")
 
 if name_filter:
